70ca47c7e99a1e4a7bf1d06da34d5d248f9b4ea2
Hash <hashiramanakamoto0@gmail.com>
2026-02-17 08:50:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6847487065-fb59b",
+  "appId": "1:97328507278:web:9a544d8c8bd509e7045cdf",
+  "apiKey": "AIzaSyCg9Wd0srYSXmGMD7E0mhFs98V08SmnEdk",
+  "authDomain": "studio-6847487065-fb59b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "97328507278"
+};
